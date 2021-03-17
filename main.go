a9850bd713486d4775e94462aecd21da28fed1e2
chikwandagames <chikwandagames@gmail.com>
2021-03-17 15:13:11 +0000

    http.Handler, basics

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// http.Handeer
+
+// Anything the implements ServerHTTP(ResponseWriter, *Request)
+// is of type handler
+/*
+	type Handler interface {
+		ServerHTTP(ResponseWriter, *Request)
+	}
+
+*/
+
+func main() {
+
+}
