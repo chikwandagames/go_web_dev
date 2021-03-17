1e388af469a15bb4584d237fc0d2d449ee1655c9
chikwandagames <chikwandagames@gmail.com>
2021-03-17 15:33:21 +0000

    ListenAndServer

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // http.Handeer
 
 // Anything the implements ServerHTTP(ResponseWriter, *Request)
@@ -11,6 +16,17 @@ package main
 
 */
 
-func main() {
+type hotdog int
 
+func main() {
+	var d hotdog
+	// ListenAndServe(addr string, handler Handler)
+	http.ListenAndServe(":8080", d)
+
+}
+
+// Attaching the ServerHTTP(ResponseWriter, *Request) function to
+// type hotdog makes hotdog be of type http.Handler
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Any code you want in this function")
 }
