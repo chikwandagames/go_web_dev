4218ea8dede9e6146635e3fb117d0f96d730911e
chikwandagames <chikwandagames@gmail.com>
2021-03-18 11:38:02 +0000

    Handle, vs HandleFunc

--- main.go
+++ main.go
@@ -17,10 +17,16 @@ import (
 
 func main() {
 
-	// Because handle func take a function with the signature
-	// myFunc(w http.ResponseWriter, r *http.Request)
-	http.HandleFunc("/dog/", d)
-	http.HandleFunc("/cat", c)
+	// Because handle take a http.HandlerFunc type, we can cast d and c
+	// to type HandlerFunc, http.HandlerFunc implements
+	// ServerHTTP(ResponseWriter, *Request), there for it is a handler
+	// Handle() wants a handler
+
+	// the cast is possible because d and c have the same signature as
+	// ServerHTTP
+
+	http.Handle("/dog/", http.HandlerFunc(d))
+	http.Handle("/cat", http.HandlerFunc(c))
 
 	// If you pass nil, ListenAndServe will use DefaultServeMux
 	http.ListenAndServe(":8080", nil)
