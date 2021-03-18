2b908ab7021dc2ed5fa4f72af2f39d5e48618f06
chikwandagames <chikwandagames@gmail.com>
2021-03-18 10:00:07 +0000

    ServeMux

--- main.go
+++ main.go
@@ -16,21 +16,30 @@ import (
 */
 
 type hotdog int
+type hotcat int
 
 func main() {
 	var d hotdog
+	var c hotcat
 
-	http.ListenAndServe(":8080", d)
+	// NewServeMux allocates and returns a new ServeMux
+	// func NewServeMux() *ServeMux { return new(ServeMux)}
+	// Therefore mux is of type *ServeMux
+	mux := http.NewServeMux()
+	// The trailing / at the end of the path makes a difference
+	// "/dog/" will catch  /dog/something/another
+	// "/cat" will NOT catch /cat/something
+	mux.Handle("/dog/", d)
+	mux.Handle("/cat", c)
+	http.ListenAndServe(":8080", mux)
 }
 
 // Attaching the ServerHTTP(ResponseWriter, *Request) function to
 // type hotdog makes hotdog be of type http.Handler
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/dog":
-		io.WriteString(w, "Hello Chihuahua")
-	case "/cat":
-		io.WriteString(w, "Hello Sphynx")
+	io.WriteString(w, "Hello Chihuahua")
+}
 
-	}
+func (m hotcat) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello Sphynx")
 }
