218f51bbfaf3f2db83202cbb17350245200cb2cb
chikwandagames <chikwandagames@gmail.com>
2021-03-19 17:10:24 +0000

    POST, passing form value

--- main.go
+++ main.go
@@ -1,33 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 )
 
-// visit link
-// localahost:8080/?key=whatever
-
 func main() {
 	http.HandleFunc("/", foo)
-	// http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
-
-	// FormValue returns the firest value for the name component of the query
-	// POST and PUT body params take precedence over URL query string values.
-	// FormValues calls ParseMultipartForm and ParseForm if necessary and ignores
-	// errors returned by these functions
-	// If key is NOT present, FormValues returns an empty string.
-	// To access multiple values of the same key, call ParseForm and then
-	// inspect Request.Form directly
-
-	// key is from the query i.e. localahost:8080/?key=whatever
-	// Here we use FormValue() to retrieve the value from the URL
-	v := r.FormValue("key")
-	// Set the content-type to html so we can use the <em> tag
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "Your query string value is: <em> %v </em>", v)
+func foo(w http.ResponseWriter, req *http.Request) {
+	// Retreev
+	v := req.FormValue("first_name")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// When we use POST, form data is sent via the request body
+	// When you hit submit, the page refresh renders the page with first_name v
+	io.WriteString(w, `
+	<form method="POST">
+	 <input type="text" name="first_name" autocomplete="off" placeholder="first name">
+	 <input type="submit">
+	</form>
+	<br> <p> your name is: `+v)
 }
