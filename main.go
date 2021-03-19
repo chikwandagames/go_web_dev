bfc379bc129c44cace1e00471e1d3fd0306811ce
chikwandagames <chikwandagames@gmail.com>
2021-03-19 15:16:30 +0000

    http.NotFoundHandler, favicon error handling

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", dog)
-
-	// http://localhost:8080/resources/ will list all files in the assets folder
-	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func dog(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	//
-
-	// in the <img> tag /resources/ referes to the /resources route
-	// toby.jpg is the file in the assets folder
-	// http.StripPrefix("/resources") will remover "/resources" from
-	// src="/resources/toby.jpg" and leave just /toby.jpg
-	// http.FileServer() will look for anything in the ./assets folder
-	// in the current dir in this case /toby.jpg
-
-	// So technically ./assets will be concatenated to /toby by http.FileServer
-	// After http.StripPrefix has removed /resources from src="/resources/toby.jpg"
-	io.WriteString(w, `<img src="/resources/toby.jpg">`)
-	io.WriteString(w, `<img src="/resources/auba.jpg">`)
+func foo(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL.Path)
+	fmt.Fprintln(w, "go look at your terminal")
 }
