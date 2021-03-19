4d6bb3a6723178b5606f52757d9b1d11049a8da6
chikwandagames <chikwandagames@gmail.com>
2021-03-19 17:29:46 +0000

    GET, passing for values

--- main.go
+++ main.go
@@ -12,15 +12,13 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
-	// Retreev
 	v := req.FormValue("first_name")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// When we use POST, form data is sent via the request body
-	// When you hit submit, the page refresh renders the page with first_name v
+	// On submit the form data will be passed to server via the URL as query strings
 	io.WriteString(w, `
-	<form method="POST">
-	 <input type="text" name="first_name" autocomplete="off" placeholder="first name">
-	 <input type="submit">
+	<form method="GET">
+	<input type="text" name="first_name" autocomplete="off" placeholder="first name">
+	<input type="submit">
 	</form>
 	<br> <p> your name is: `+v)
 }
