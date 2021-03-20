38f637d298435ca1433e57116d266069cb3c5962
chikwandagames <chikwandagames@gmail.com>
2021-03-20 21:59:12 +0000

    redirect, http.StatusTemporaryRedirect

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ func init() {
 func main() {
 	http.HandleFunc("/", foo)
 	http.HandleFunc("/bar", bar)
-	// Executes a template at, index.html
 	http.HandleFunc("/barred", barred)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
@@ -25,16 +24,16 @@ func foo(w http.ResponseWriter, req *http.Request) {
 	fmt.Print("Your request method at foo: ", req.Method, "\n\n")
 }
 
-func bar(w http.ResponseWriter, req *http.Request) {
-	// The form in barred (index.html) is submitted to bar handler
-	// <form method="POST" action="/bar">
-	fmt.Println("Your request method at bar:", req.Method)
+// The form in barred (index.html) is submitted to bar handler
+// <form method="POST" action="/bar">
 
+func bar(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at bar:", req.Method)
 	// process form submission here
 
-	// Becauser we are using http.StatusSeeOther 303 status code, the method
-	// will be changed to GET
-	http.Redirect(w, req, "/", http.StatusSeeOther)
+	// The difference from using http.StatusSeeOthe to http.StatusTemporaryRedirect
+	// is that the method does not change to GET it stays as POST
+	http.Redirect(w, req, "/", http.StatusTemporaryRedirect)
 }
 
 func barred(w http.ResponseWriter, req *http.Request) {
