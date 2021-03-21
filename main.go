a52c3c8a4d109ffd701e49abdc553dd31eefc7a0
chikwandagames <chikwandagames@gmail.com>
2021-03-21 18:11:32 +0000

    cookies, reading and writing

--- main.go
+++ main.go
@@ -2,44 +2,39 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
-}
-
 func main() {
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/bar", bar)
-	http.HandleFunc("/barred", barred)
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func foo(w http.ResponseWriter, req *http.Request) {
-	fmt.Print("Your request method at foo: ", req.Method, "\n\n")
+func set(w http.ResponseWriter, req *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "some value inside the cookie",
+		Path:  "/",
+	})
+	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(w, "in chrome go to: dev tools / application / cookies")
 }
 
-// The form in barred (index.html) is submitted to bar handler
-// <form method="POST" action="/bar">
+func read(w http.ResponseWriter, req *http.Request) {
 
-func bar(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Your request method at bar:", req.Method)
-	// process form submission here
+	// Cookie() reads a cookie, takes the name of the cookie
 
-	// If you hit bar once for the first time, the browser will not
-	// allow you to hit bar again,
-	// each time you hit bar the browser will automatically redirect you
-	// to foo "/" because of http.StatusMovedPermanently.
-	// You would need to clear the cache to access bar again
-	http.Redirect(w, req, "/", http.StatusMovedPermanently)
-}
+	// Cookie returns the named cookie provided in the request or
+	// ErrNoCookie if not found.
+	// If multiple cookies match the given name, only one cookie will
+	// be returned.
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
 
-func barred(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Your request method at barred:", req.Method)
-	tpl.ExecuteTemplate(w, "index.html", nil)
+	fmt.Fprintln(w, "YOUR COOKIE:", c)
 }
