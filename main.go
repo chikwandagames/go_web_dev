106153356a72cae6a79d21bc8e3c5d479598a2d1
chikwandagames <chikwandagames@gmail.com>
2021-03-25 10:06:06 +0000

    Base64, no custom encoding standard

--- main.go
+++ main.go
@@ -13,11 +13,7 @@ import (
 func main() {
 	s := "Love is but a song to sing Fear's the way we die You can make the mountains ring Or make the angels cry Though the bird is on the wing And you may not know why Come on people now Smile on your brother Everybody get together Try to love one another Right now"
 
-	// The encoding standard you want to use
-	encodeStd := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
-	// Pass th string as a slice of bytes
-	s64 := base64.NewEncoding(encodeStd).EncodeToString([]byte(s))
+	s64 := base64.StdEncoding.EncodeToString([]byte(s))
 
 	fmt.Println(len(s))
 	fmt.Println(len(s64))
