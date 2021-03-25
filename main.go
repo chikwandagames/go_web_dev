c3d05cb6ef3b5e834d26e213cb11c9c70bb123a6
chikwandagames <chikwandagames@gmail.com>
2021-03-25 10:01:20 +0000

    Base64, encode using custom encoding standard

--- main.go
+++ main.go
@@ -1,38 +1,26 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
-	"io"
 )
 
-// HMAC, Hash-based Message Authentication
-// The same input should produce the same output
-// How we can use HMAC
-// If we store a value in a users machine, and want to verify that
-// number has not been tempered with,
-// 1. We can take the value we want to store, run it through HMAC + Private key,
-//    this creates a hash
-// 2. We can then then take the value + hash, pin the 2 together, store on them
-//    on the users machine
-// 3. When we get the value and hash from the user, we can run it throut the
-//    hash algorithm with our private key, this produces a hash
-// 4. We can then compare that hash to the one stored on the users machine
-// These should match if values are not tempered with
+// Base64
+// For eg, if you want to store text in double qoutes into a cookie,
+// e.g. JSON, cookies don't like double qoutes, best way is encode the JSON
+// or string in Base64, put that in the cookie, then decode to use
 
 func main() {
-	c := getCode("test@example.com")
-	fmt.Println(c)
-	c = getCode("test@exampl.com")
-	fmt.Println(c)
-}
+	s := "Love is but a song to sing Fear's the way we die You can make the mountains ring Or make the angels cry Though the bird is on the wing And you may not know why Come on people now Smile on your brother Everybody get together Try to love one another Right now"
 
-func getCode(s string) string {
-	// hmac.New(), takes a shar256 hash + a private key
-	// and returns a hash
-	h := hmac.New(sha256.New, []byte("ourkey"))
-	// Here we write the passed string into the hash
-	io.WriteString(h, s)
-	return fmt.Sprintf("%x", h.Sum(nil))
+	// The encoding standard you want to use
+	encodeStd := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+	// Pass th string as a slice of bytes
+	s64 := base64.NewEncoding(encodeStd).EncodeToString([]byte(s))
+
+	fmt.Println(len(s))
+	fmt.Println(len(s64))
+	fmt.Println(s)
+	fmt.Println(s64)
 }
