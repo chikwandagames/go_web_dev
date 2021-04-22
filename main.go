aa94e3af02bd0c5ecbbe8b95686a595cc6993f19
chikwandagames <chikwandagames@gmail.com>
2021-04-22 16:00:23 +0100

    update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 /*
@@ -47,14 +48,38 @@ func foo(w http.ResponseWriter, req *http.Request) {
 
 // Retrieves the Data from the context
 // dbAccess returns a tuple
-func dbAccess(ctx context.Context) (int, string) {
-	// .(int) is an assertion, asserting that ctx.Value("userID") is an int
-	uid := ctx.Value("userID").(int)
-	name := ctx.Value("fname").(string)
-	/*
-		acccess DB ...
-	*/
-	return uid, name
+func dbAccess(ctx context.Context) (int, error) {
+
+	// Set to timeout after one second
+	// WithTimeout returns a cancel func
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	// Create a chanel of type int
+	ch := make(chan int)
+
+	go func() {
+		// ridiculous long running task
+		uid := ctx.Value("userID").(int)
+		time.Sleep(10 * time.Second)
+
+		// check to make sure we're not running in vain
+		// if ctx.Done() has
+		if ctx.Err() != nil {
+			return
+		}
+
+		ch <- uid
+	}()
+
+	select {
+	// True if the channel has been cancelled
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	// If the channel produces a value
+	case i := <-ch:
+		return i, nil
+	}
 }
 
 func bar(w http.ResponseWriter, req *http.Request) {
